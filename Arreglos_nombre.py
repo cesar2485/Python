a561ecd6cb4f03478b8e174292c416e0e5f1f72f
cesar2485 <cesmartinez@outlook.com>
2022-04-30 14:56:06 -0500

    se realiza ordemaniento del codigo

--- Arreglos_nombre.py
+++ Arreglos_nombre.py
@@ -1,13 +1,24 @@
 
 n = int(input("Digite cantidad de estudiantes: ")) #  Se solicita el número de estudiantes y el dato entrante se asigna a n
-codigo = [ (input("Digite codigo: T000"))  for x in range(n)]
+codigo = [ (input("Digite codigo: "))  for x in range(n)]
 nota = [eval(input("Digite Nota: ")) for x in range(n)]
-print("Nota Maxima:", max(nota), " Nota Minima:", min(nota)) # se imprime nota maxima y minima
-promedio = sum(nota)/n # Calculo de promedio
+
+# se imprime nota maxima y minima
+print("Nota Maxima:", max(nota), " Nota Minima:", min(nota)) 
+
+# Calculo de promedio
+promedio = sum(nota)/n 
+print("Promedio es: ", promedio)
+
+# Cuantos estudiantes Aprobaron y Cuantos Reprobaron
 aprobaron = [nota for nota in nota if nota >= 3] 
 reprobaron = [nota for nota in nota if nota < 3]
-print("Promedio es: ", promedio)
 print("Aprobaron: ", len(aprobaron), "  Reprobaron: ", len(reprobaron))
-#print(codigo)
-#print(nota)
+
+# relacion entre codigos y notas
+print("----relacion codigo-notas----" )
+for indice in range(0, len(codigo)): # 
+    print(codigo[indice], nota[indice])
+
+ 
 
