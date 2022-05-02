34e837bef615c5d194c30f3500206c81a73088dd
cesar2485 <cesmartinez@outlook.com>
2022-05-02 17:24:34 -0500

    cambios finales

--- Arreglos_Pedro.py
+++ Arreglos_Pedro.py
@@ -1,9 +1,9 @@
-
-n = int(input("Digite cantidad de estudiantes: ")) #  Se solicita el número de estudiantes y el dato entrante se asigna a n
+#  Se solicita el número de estudiantes y el dato entrante se asigna a n
+n = int(input("Digite cantidad de estudiantes: ")) 
 codigo = [ (input("Digite codigo: "))  for x in range(n)]
 nota = [eval(input("Digite Nota: ")) for x in range(n)]
 
-# se imprime nota maxima y minima
+# nota maxima y minima
 print("Nota Maxima:", max(nota), " Nota Minima:", min(nota)) 
 
 # Calculo de promedio
@@ -15,30 +15,44 @@ aprobaron = [nota for nota in nota if nota >= 3]
 reprobaron = [nota for nota in nota if nota < 3]
 print("Aprobaron: ", len(aprobaron), "  Reprobaron: ", len(reprobaron))
 
+# se inicia variable para contar 
+# los estudiantes quedaron en cada una de las categorias
+a = 0
+b = 0
+c = 0
+d = 0
+
 # relacion entre codigos y notas
 print("----Relacion codigo-notas----" )
 print("Codigo", "Nota" )
 for valor_a, valor_b in zip(codigo, nota): 
-#for indice in nota :
     if 0.5 <= valor_b < 2.0:
        print(valor_a, valor_b, " Insuficiente")
-       insuficiente = [int(valor_b)]
+       a = a + 1
     elif 2.0 <= valor_b < 3.0:
         print(valor_a, valor_b, " Suficiente")
-        suficiente = [int(valor_b)]
+        b = b + 1
     elif 3.0 <= valor_b < 4.0:
         print(valor_a, valor_b, " Bien")
+        c = c + 1
     elif 4.0 <= valor_b <= 5.0:
         print(valor_a, valor_b, " Muy Bien")
-
-# cuantos estudiantes quedaron en cada una de las categorias
-print("Suficiente: ", len(suficiente))
-print("Insuficiente: ", len(insuficiente))
+        d = d + 1
+print(valor_a, valor_b)
+#  impresion de cuantos estudiantes quedaron en cada una de las categorias
+print("Insuficiente: ", a)
+print("Suficiente: ", b)
+print("Bien: ", c)
+print("Muy Bien: ", d)
 
 # Porcentajes de aprobados y reprobados
-porcentaje1 = (n/100)*len(aprobaron)
-porcentaje2 = (n/100)*len(reprobaron)
-print("Aprobados: ", porcentaje1,"%",  "  Reprobados: ", porcentaje2,"%")
+porcentaje1 = (100/n)*len(aprobaron)
+porcentaje2 = (100/n)*len(reprobaron)
+print("Pocentaje de Aprobados: ", porcentaje1,"%",  "  Porcentaje de Reprobados: ", porcentaje2,"%")
+
+# estudiantes que obtuvieron las mas alta nota
+
+
 
 
  
---
+++ prueba.py
@@ -0,0 +1,5 @@
+codigo= ["t0023", "t0035", "t0054", "t0067", "t0056"]
+notas = [3.5, 4.8, 2.8, 1.3, 4.8]
+
+alumno = [codigo,notas]
+print(alumno[0][0])
